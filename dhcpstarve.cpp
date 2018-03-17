2d8cdde53e292e0e03f8aa3538049d25b7233602
Adam Šulc <xsulca00@stud.fit.vutbr.cz>
2018-03-17 10:55:10 +0100

    Init commit

---
+++ dhcpstarve.cpp
@@ -0,0 +1,40 @@
+extern "C" {
+#include <net/if.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+}
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+vector<string> make_args(int argc, char* argv[]) {
+    vector<string> args;
+    for (int i {0}; i != argc; ++i) args.push_back(argv[i]);
+    return args;
+}
+
+int main(int argc, char* argv[]) {
+    vector<string> args {make_args(argc, argv)};
+    if (args.size() != 3) throw runtime_error{"Usage: " + args[0] + " -i interface"};
+
+    const string interface {args.back()};
+
+    ifreq ifr {};
+    ifr.ifr_addr.sa_family = AF_INET;
+    copy_n(interface.cbegin(), IFNAMSIZ-1, &ifr.ifr_name[0]);
+    ifr.ifr_name[IFNAMSIZ-1] = '\0';
+
+    cout << "Interface: " << ifr.ifr_name << '\n';
+
+    int socket {::socket(AF_INET, SOCK_DGRAM, 0)};
+    
+    if (ioctl(socket, SIOCGIFMTU, &ifr) == -1) throw system_error{errno, generic_category()}; 
+    cout << "MTU: " << ifr.ifr_mtu << '\n';
+
+    close(socket);
+}
